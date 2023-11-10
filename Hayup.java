26b274b548226c737495e93d940ae18cf455a8ff
realdjun2 <realdjunodon@gmail.com>
2023-11-10 10:28:10 +0000

    first commit

---
+++ Hayup.java
@@ -0,0 +1,12 @@
+
+public class Hayup {
+   String name;
+    Hayup (String name){
+        this.name = name;
+    }
+    void bite(){
+        System.out.println( name+"bitting");
+    }
+    void eat(){
+        System.out.println( name+"eating");
+}
